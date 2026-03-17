908dfde08101bc7c5634a975668ce49b870818d9
meldyssey <meldyssey15@gmail.com>
2026-03-17 14:14:56 +0900

    feat: React 앱 진입점 및 Babel 설정 추가
    React 컴포넌트 렌더링을 위한 기본 구조 구성
    
    - src/index.js 추가 (ReactDOM.render 진입점)
    - src/Root.js 추가 (루트 컴포넌트)
    - .babelrc 추가 (ES6/JSX → ES5 변환 설정)
    - webpack.config.js에 babel-loader 및 entry 변경
    - .vscode/settings.json 추가 (Prettier 포맷터 설정)
    
    영향 범위: src/index.js, src/Root.js, .babelrc, webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,40 @@
 const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
-
 module.exports = {
   // 웹팩이 빌드할 파일을 알려주는 역할
-  entry: "./src/test.js",
+  entry: "./src/index.js",
   // 웹팩에서 빌드를 완료하면 output에 명시되어 있는 정보를 통해 빌드 파일을 생성
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname + "/build")
+    path: path.resolve(__dirname + "/build"),
   },
   // 웹팩 빌드 옵션으로 production은 최적화되어 빌드되어지는 특징을 가지고 있고 development는 빠르게 빌드하는 특징, none 같은 경우는 아무 기능 없이 웹팩으로 빌드
   mode: "none",
   module: {
     rules: [
       {
+        test: /\.(js|jsx)$/,
+        exclude: "/node_modules",
+        use: ["babel-loader"],
+      },
+      {
         test: /\.html$/,
         use: [
           {
             // 로더는 html 파일을 읽었을 때 html-loader를 실행하여 웹팩이 이해할 수 있게 하고 options 으로는 minimize(줄바꿈 없음)라는 코드 최적화 옵션을 사용
             loader: "html-loader",
-            options: { minimize: true }
-          }
-        ]
-      }
-    ]
+            options: { minimize: true },
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     // html-webpack-plugin은 웹팩이 html 파일을 읽어서 html 파일을 빌드
     new HtmlWebPackPlugin({
-			template: './public/index.html', // public/index.html 파일을 읽는다.
-      filename: 'index.html' // output으로 출력할 파일은 index.html 이다.
-    })
-  ]
+      template: "./public/index.html", // public/index.html 파일을 읽는다.
+      filename: "index.html", // output으로 출력할 파일은 index.html 이다.
+    }),
+  ],
 };
