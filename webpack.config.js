ec6e788dc583e4198ada0eb1c8b5f18d6ddd130b
meldyssey <meldyssey15@gmail.com>
2026-03-17 14:07:22 +0900

    chore: HtmlWebPackPlugin 및 HTML 로더 설정 추가
    HTML 파일 번들링을 위한 webpack 설정 추가
    
    - webpack.config.js에 html-loader 및 HtmlWebPackPlugin 설정 추가
    - public/index.html 템플릿 파일 추가
    - 빌드 결과물 build/index.html 추가
    
    영향 범위: webpack.config.js, public/index.html, build/index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
 
 module.exports = {
   // 웹팩이 빌드할 파일을 알려주는 역할
@@ -9,5 +11,26 @@ module.exports = {
     path: path.resolve(__dirname + "/build")
   },
   // 웹팩 빌드 옵션으로 production은 최적화되어 빌드되어지는 특징을 가지고 있고 development는 빠르게 빌드하는 특징, none 같은 경우는 아무 기능 없이 웹팩으로 빌드
-  mode: "none"
+  mode: "none",
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            // 로더는 html 파일을 읽었을 때 html-loader를 실행하여 웹팩이 이해할 수 있게 하고 options 으로는 minimize(줄바꿈 없음)라는 코드 최적화 옵션을 사용
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    // html-webpack-plugin은 웹팩이 html 파일을 읽어서 html 파일을 빌드
+    new HtmlWebPackPlugin({
+			template: './public/index.html', // public/index.html 파일을 읽는다.
+      filename: 'index.html' // output으로 출력할 파일은 index.html 이다.
+    })
+  ]
 };
